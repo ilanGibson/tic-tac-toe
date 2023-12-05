9d65ef025354568eff820caa1c371fa4c4561075
ilanGibson <ilanjgibson@gmail.com>
2023-12-04 17:58:21 -0700

    checkForWinner is working fairly well

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function Gameboard() {
     const placeMarker = (row, column, player) => {
         var choseCell = board[row][column];
         if (choseCell.getMarker() === "") {
-            choseCell.addMarker(player);
+            choseCell.addMarker(player, row, column);
         } else {
             console.log("invalid chose");
         }
@@ -34,10 +34,28 @@ function Gameboard() {
     // find out which patterns are winning
     // find out how to detect tie
     const checkForWinner = (playerMarker) => {
-        const filteredCells = board.filter(row => {
+        const filteredCells = board.map(row => {
             return row.filter(num => num.getMarker() === playerMarker);
         });
+
         console.log(filteredCells);
+        var row = [];
+        var column = [];
+        for (i of filteredCells) {
+            for (j of i) {
+                row.push(j.getCoordinates()[0]);
+                column.push(j.getCoordinates()[1]);
+            }
+        }
+
+        console.log(row);
+
+
+        // loop through row and column
+        // compare first value to the remaining values and return winning if 3 match
+        // diagonals will be seperate case
+        // if length < 3 return false
+        // maybe have check for ties as different function?
     };
 
     return {getBoard, placeMarker, checkForWinner};
@@ -46,18 +64,30 @@ function Gameboard() {
 
 function Cell() {
     let value = "";
+    let cellRow = "";
+    let cellColumn = "";
 
-    const addMarker = (player) => {
+    const addMarker = (player, row, column) => {
         value = player;
+        setCoordinates(row, column);
     };
 
     const getMarker = () => value;
 
-    return {addMarker, getMarker};
+    const setCoordinates = (row, column) => {
+        cellRow = row;
+        cellColumn = column
+    }
+
+    const getCoordinates = () => [cellRow, cellColumn];
+
+    return {addMarker, getMarker, getCoordinates};
 }
 
 var temp = Gameboard();
 temp.placeMarker(1, 2, "X");
+temp.placeMarker(0, 0, "X");
+temp.placeMarker(0, 1, "X");
 var board = temp.getBoard();
 console.log(board[1][2].getMarker());
 temp.checkForWinner("X")
